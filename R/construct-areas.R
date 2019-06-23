06f728d241260b3178ea4fd7811c0be5ad17fcf3
atusy <30277794+atusy@users.noreply.github.com>
2019-06-23 14:42:25 +0900

    update: construct_areas
    - Returns "grid_areas" class object
    - Returns as is if input has "grid_areas" class
    - Deleted construct_areas.character because .default works

--- R/construct-areas.R
+++ R/construct-areas.R
@@ -10,6 +10,12 @@ construct_areas <- function(x = NULL, ...) UseMethod("construct_areas")
 #' @noRd
 collapse_areas <- function(x) collapse(paste0("'", x, "'"))
 
+as_grid_areas <- function(x) structure(x, class = c("grid_areas", class(x)))
+
+#' @rdname construct_areas
+#' @export
+construct_areas.grid_areas <- function(x = NULL, ...) x
+
 #' @rdname construct_areas
 #' @export
 construct_areas.default <- function(x = NULL, ...) {
@@ -17,17 +23,17 @@ construct_areas.default <- function(x = NULL, ...) {
   if (!(is.atomic(x) & is.vector(x))) {
     stop("x must be either NULL, an atomic vector, a matrix, or a data frame")
   }
-  collapse_areas(x)
+  as_grid_areas(collapse_areas(x))
 }
 
 #' @rdname construct_areas
 #' @export
-construct_areas.character <- function(x, ...) collapse_areas(x)
+construct_areas.data.frame <- function(x, ...) {
+  as_grid_areas(collapse_areas(Reduce(paste, x)))
+}
 
 #' @rdname construct_areas
 #' @export
-construct_areas.data.frame <- function(x, ...) collapse_areas(Reduce(paste, x))
-
-#' @rdname construct_areas
-#' @export
-construct_areas.matrix <- function(x, ...) collapse_areas(apply(x, 1L, collapse))
+construct_areas.matrix <- function(x, ...) {
+  as_grid_areas(collapse_areas(apply(x, 1L, collapse)))
+}
