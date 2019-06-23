bb8e4f59cbd8c543c9bdf1035cd79f7789bb418b
atusy <30277794+atusy@users.noreply.github.com>
2019-06-23 13:59:46 +0900

    fix+feat: grid_item
    - fix wrong name of a CSS property
    - Use collapse instead of paste0 so that style becomes NULL
    instead of ": ;" when no valid arguments are given.
    - Added an argument, col, as an alias of column

--- R/grid-item.R
+++ R/grid-item.R
@@ -5,6 +5,8 @@
 #'  replaced by underscores. For example, the `row_start` argument is equivalent
 #'  to the CSS's `grid-row-start` property. Specify a string as a value for the
 #'  corresponding CSS property.
+#' @param col
+#'  An alias of column
 #' @param justify_self,align_self
 #'  Strings to specify values for the CSS's
 #'  [`justify-self`](https://developer.mozilla.org/en-US/docs/Web/CSS/justify-self)
@@ -24,7 +26,8 @@ grid_item <- function(
   row = NULL,
   column_start = NULL,
   column_end = NULL,
-  column = NULL,
+  col = NULL,
+  column = col,
   area = NULL,
   justify_self = NULL,
   align_self = NULL,
@@ -34,7 +37,7 @@ grid_item <- function(
 ) {
   div(
     ...,
-    style = paste0(construct_style(
+    style = collapse(construct_style(
       "grid-row-start" = row_start,
       "grid-row-end" = row_end,
       "grid-row" = row,
@@ -43,7 +46,7 @@ grid_item <- function(
       "grid-column" = col,
       "grid-area" = area,
       "justify-self" = justify_self,
-      "align-sef" = align_self,
+      "align-self" = align_self,
       "z-index" = z_index,
       order = order
     ), style)
